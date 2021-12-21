a3682182b9cc71987e6d41da2e554ffc41fbadf3
Robbe Derks <robbe.derks@gmail.com>
2021-12-21 16:51:39 +0100

    THIS WORKS!

--- bottle_patch/hook.c
+++ bottle_patch/hook.c
@@ -11,84 +11,72 @@
 
 // Used pointers in the original code:
 volatile uint8_t *return_val_ptr = (volatile uint8_t *)0x0000;
-volatile uint8_t *state_flags_ptr = (volatile uint8_t *)0x0011;
-volatile uint8_t *input_arg_L_ptr = (volatile uint8_t *)0x00c0;
-volatile uint8_t *input_arg_H_ptr = (volatile uint8_t *)0x00c1;
 volatile uint8_t *beep_time_L_ptr = (volatile uint8_t *)0x00be;
 volatile uint8_t *beep_time_H_ptr = (volatile uint8_t *)0x00bf;
 
-extern void delay(void) __at(0x75c8);
 extern void read_port_b_e(void) __at(0x67d2);
 extern void beep(void) __at(0x7118);
 
-#define RETURN_VAL_HOOKED 1U
-#define RETURN_VAL_REGULAR 0U
+// Globals
+uint16_t fake_dispensing_counter;
+uint8_t handsfree;
+
+#define INIT_MAGIC 0xA5A5
+volatile uint16_t init_checker;
 
 // Put the patch function at a known empty address
 // to make it easy to find in the binary
-uint8_t patch() __at(0x8000);
-uint8_t patch() {
-  // Are we dispensing cold water already?
-  read_port_b_e();
-  if (((*return_val_ptr) & 0x2) != 0) {
-    uint8_t handsfree = 0;
+void patch() __at(0x8000);
+void patch() {
+  // We wrap the button readout function to be able to fake button presses
+  // in the dispensing logic.
 
-    // Yes -> short beep and start the delay!
+  // Init variables if not done so already.
+  // Since this is in RAM, everything will have an unknown value at startup
+  if (init_checker != INIT_MAGIC) {
+    fake_dispensing_counter = 0;
+    handsfree = 0;
+    init_checker = INIT_MAGIC;
+  }
+
+  // Read real status
+  read_port_b_e();
+
+  if (fake_dispensing_counter == 0 && ((*return_val_ptr) & 0x3) == 0x3) {
+    // Pressing both buttons, so we start the fake dispense and beep
+    fake_dispensing_counter = 0xf000;
+    handsfree = 0U;
+
     *beep_time_L_ptr = (uint8_t) 0x05;
     *beep_time_H_ptr = (uint8_t) 0x01;
     beep();
-
-    for (uint8_t i=0; i<100; i++) {
-      *input_arg_L_ptr = (uint8_t) 0xff;
-      *input_arg_H_ptr = (uint8_t) 0x0;
-      delay();
-
-      // Check that we didn't press a button
-      read_port_b_e();
-      uint8_t button_pressed = ((*return_val_ptr) & 0x3);
-      if (handsfree == 0) {
-        // We're still waiting on a button release
-        if (button_pressed == 0) {
-          handsfree = 1;
-        }
-      } else {
-        // Buttons have been released
-        if (button_pressed != 0) {
-          // We pressed a button again: cancel!
-          break;
-        }
-      }
-
-      // Kick watchdog
-      CLRWDT();
-    }
-
-    return RETURN_VAL_HOOKED;
   }
 
-  // If not, we run the code we overwrote and return back into the hooked function
-  PORTD |= (1 << 3);
+  if (fake_dispensing_counter > 0) {
+    if ((handsfree == 0) && (((*return_val_ptr) & 0x3) == 0)) {
+      // We released the buttons
+      handsfree = 1U;
+    }
 
-  *input_arg_L_ptr = (uint8_t) 0x9b;
-  *input_arg_H_ptr = (uint8_t) 0x0;
-  delay();
+    if ((handsfree != 0U) && (((*return_val_ptr) & 0x3) != 0)) {
+      // We cancelled by pressing a button
+      *beep_time_L_ptr = (uint8_t) 0x05;
+      *beep_time_H_ptr = (uint8_t) 0x00;
+      beep();
 
-  return RETURN_VAL_REGULAR;
-}
+      fake_dispensing_counter = 0U;
+      return;
+    }
 
-// This is the code we'll replace the beginning of "dispense_room_temp_water" with
-uint8_t hook() __at(0x9000);
-uint8_t hook() {
-  if (patch() == 1U) {
-    return 0;
-  } else {
-    // just an easy instruction to make sure the if is not optimized out
-    return 1;
+    // Fake only cold water button
+    *return_val_ptr = ((*return_val_ptr & 0xf0) | (1 << 1));
+
+    fake_dispensing_counter--;
   }
 }
 
 // xc8 does not generate .o files, so we need to have main()
 void main() __at(0x10);
 void main() {
-  *input_arg_L_ptr = hook();
+  patch();
 }
